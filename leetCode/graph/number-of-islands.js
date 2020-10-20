952792f99a3d5d0967bed1513aa0e4e39393fa24
seungho <50465901+csh9066@users.noreply.github.com>
2020-10-20 14:01:23 +0900

    Add number-of-islands

---
+++ leetCode/graph/number-of-islands.js
@@ -0,0 +1,53 @@
+// // 문제
+// // 1을 육지로 0을 물로 가정한 2D m*n 그리드 맵이 주어졌을때, 섬의 개수를 계산하라
+
+// Input: grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// Output: 1
+
+// Input: grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// Output: 3
+
+var numIslands = function (grid) {
+  function dfs(grid, i, j) {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[0].length ||
+      grid[i][j] !== '1'
+    ) {
+      return;
+    }
+
+    // 탐색한 인덱스는 0으로 바꿈
+    grid[i][j] = 0;
+
+    // 상하좌우 탐색
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
+  }
+
+  let count = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '1') {
+        dfs(grid, i, j);
+        count++;
+      }
+    }
+  }
+  return count;
+};
