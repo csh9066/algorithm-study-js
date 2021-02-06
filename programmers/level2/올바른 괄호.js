2aaa5f2946da214ecd6e3bd12a55dbfd933d742d
seungho <50465901+csh9066@users.noreply.github.com>
2021-02-06 13:03:50 +0900

    Add 올바른 괄호

---
+++ programmers/level2/올바른 괄호.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  const stack = [];
+
+  if (s[0] === ')') {
+    return false;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      stack.push(s[i]);
+    } else {
+      if (!stack.length) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+}
